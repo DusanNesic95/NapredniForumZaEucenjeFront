8040cd7866f437086753202612eceb62a90d1a6e
DusanNesic95 <dusannesic001@gmail.com>
2017-04-10 11:33:47 +0200

    Sredjeni stilovi pocetnih stranica za sve uloge.

--- controllers/HomeController.js
+++ controllers/HomeController.js
@@ -17,7 +17,93 @@ app.controller('homeController', ['$scope', 'funkcije', '$localStorage', '$http'
 	}
 
 	$scope.idiNaTeme = function() {
-		window.location = "#/temaprikazstudent";	
+		window.location = "#/temaprikazstudent";
+	}
+
+	$scope.aktivirajPocetnaStudent = function() {
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("teme").className = "treeview";
+		document.getElementById("pocetna").className = "active treeview";
+	}
+
+	$scope.aktivirajPredmetiStudent = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("teme").className = "treeview";
+		document.getElementById("predmeti").className = "active treeview";
+	}
+
+	$scope.aktivirajTemeStudent = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("teme").className = "active treeview";
+	}
+
+	$scope.aktivirajPocetnaProfesor = function() {
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("teme").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("pocetna").className = "active treeview";
+	}
+
+	$scope.aktivirajPredmetiProfesor = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("teme").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("predmeti").className = "active treeview";
+	}
+
+	$scope.aktivirajTemeProfesor = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("teme").className = "active treeview";
+	}
+
+	$scope.aktivirajMaterijaliProfesor = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("teme").className = "treeview";
+		document.getElementById("materijali").className = "active treeview";
+	}
+
+	$scope.aktivirajPocetnaAdmin = function() {
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("korisnici").className = "treeview";
+		document.getElementById("blokiranje").className = "treeview";
+		document.getElementById("pocetna").className = "active treeview";
+	}
+
+	$scope.aktivirajPredmetiAdmin = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("korisnici").className = "treeview";
+		document.getElementById("blokiranje").className = "treeview";
+		document.getElementById("predmeti").className = "active treeview";
+	}
+
+	$scope.aktivirajMaterijaliAdmin = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("korisnici").className = "treeview";
+		document.getElementById("blokiranje").className = "treeview";
+		document.getElementById("materijali").className = "active treeview";
+	}
+
+	$scope.aktivirajKorisniciAdmin = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("blokiranje").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("korisnici").className = "active treeview";
+	}
+
+	$scope.aktivirajBlokiranjeAdmin = function() {
+		document.getElementById("pocetna").className = "treeview";
+		document.getElementById("predmeti").className = "treeview";
+		document.getElementById("korisnici").className = "treeview";
+		document.getElementById("materijali").className = "treeview";
+		document.getElementById("blokiranje").className = "active treeview";
 	}
 
 }]);
